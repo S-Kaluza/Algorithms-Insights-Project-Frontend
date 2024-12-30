8763e29d1cabc0c0dd6c90f141947dc75ac6bea0
S-Kaluza <s.kaluza02@gmail.com>
2024-12-30 23:25:03 +0100

    AIPF-12 feat:theming

---
+++ src/Styles/DarkTheme.ts
@@ -0,0 +1,9 @@
+﻿import {createTheme } from '@mui/material/styles';
+
+const darkTheme = createTheme({
+    palette: {
+        mode: 'dark',
+    },
+});
+
+export default darkTheme
---
+++ src/Styles/LightTheme.ts
@@ -0,0 +1,9 @@
+﻿import {createTheme } from '@mui/material/styles';
+
+const lightTheme = createTheme({
+    palette: {
+        mode: 'light',
+    },
+});
+
+export default lightTheme
