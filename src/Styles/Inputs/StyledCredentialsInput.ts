683354143efd5f4e4481edc38129ca182ab34fba
S-Kaluza <s.kaluza02@gmail.com>
2025-01-01 21:14:32 +0100

    AIPF-9 comp:Credentials input

---
+++ src/Styles/Inputs/StyledCredentialsInput.ts
@@ -0,0 +1,29 @@
+﻿import styled from "@emotion/styled";
+
+const StyledCredentialsInput = styled.input`
+    width: 50%;
+    background: none;
+    color: ${(props) => props.theme.palette.text.primary};
+    height: 1rem;
+    font-size: 1rem;
+    border: none;
+    border-bottom: 1px solid black;
+    border-radius: 5px;
+    padding: 0 5px;
+    box-sizing: border-box;
+    line-height: 40px;
+
+    ::placeholder {
+        color: gray;
+        font-style: italic;
+        font-size: 16px;
+        text-align: left;
+    }
+
+    &:focus {
+        border-color: #007BFF;
+        outline: none;
+    }
+`;
+
+export default StyledCredentialsInput;
---
+++ src/Styles/Paragraph/StyledParagraph.ts
@@ -0,0 +1,7 @@
+﻿import styled from "@emotion/styled";
+
+export const StyledParagraph = styled.p`
+    margin-bottom: 0;
+    font-size: 0.85rem;
+    align-content: end;
+`
