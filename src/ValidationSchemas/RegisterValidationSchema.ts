57b3633b5e3be4fe954f3fc5b3592d3693c6bbbc
S-Kaluza <s.kaluza02@gmail.com>
2025-01-03 23:38:33 +0100

    AIPF-7 feat:validation schema

---
+++ src/ValidationSchemas/RegisterValidationSchema.ts
@@ -0,0 +1,17 @@
+﻿import * as yup from 'yup';
+
+const RegisterValidationSchema = yup.object().shape({
+    email: yup.string().email().required(),
+    password: yup.string()
+        .min(8)
+        .matches(/[0-9]/, 'Password requires a number')
+        .matches(/[a-z]/, 'Password requires a lowercase letter')
+        .matches(/[A-Z]/, 'Password requires an uppercase letter')
+        .matches(/\W/, 'Password requires a symbol')
+        .required(),
+    repeatPassword: yup.string()
+        .oneOf([yup.ref('password'), ''], 'Passwords must match')
+        .required('Repeat password is required')
+})
+
+export default RegisterValidationSchema;
