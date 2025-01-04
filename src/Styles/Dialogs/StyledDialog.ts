6843fb8ac2ff1bdc51880563928753b4d72c6bd5
S-Kaluza <s.kaluza02@gmail.com>
2025-01-04 01:27:29 +0100

    AIPF-11 feat:Dialog

---
+++ src/Styles/Dialogs/StyledDialog.ts
@@ -0,0 +1,11 @@
+﻿import styled from "@emotion/styled"
+import {Dialog} from "@mui/material";
+
+const StyledDialog = styled(Dialog)`
+    & > .MuiDialog-container > .MuiPaper-root {
+        background: ${props => props.theme.palette.primary.main};
+        width: 60vw;
+        height: 40vh;
+    }
+`;
+export default StyledDialog;
